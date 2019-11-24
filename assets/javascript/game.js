81e5672cabe4362350d3ccebc84dc90f27412bc5
Swarupa Popuri <swarupakarnam@yahoo.com>
2019-11-24 18:36:38 -0500

    almost completed homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,12 @@
 
 var arrayOfQuestions = [];
 var answered = false;
-var timeForTest = 5;
+var timeForTest = 10;
 var timerRunning = false;
 var questionSelected;
+var questionDisplayed = false;
+var correctAnswers = 0;
+var wrongAnswers = 0;
 
 function triviaQuestion(question, answer, option1, option2, option3) {
     this.question = question;
@@ -20,18 +23,37 @@ function initQuestions(){
     var question1 = new triviaQuestion("Tony Stark is saved from dying in outer space only with the help of who of these?", "Captain Marvel", "Thor", "Hulk","BatMan");
     var question2 = new triviaQuestion("Who does Heimdall manage to send to Earth during Thanos attack?", "Hulk", "Thor", "Iron Man","Mickey Mouse");
     var question3 = new triviaQuestion("Where is Loki's sceptre ultimately located?", "Sokovia", "Norway", "Scotland","South Africa");
-    var question4 = new triviaQuestion("With what is Loki able to control minds?", "Sceptre", "Stones", "Weapon","using his hands");
+    var question4 = new triviaQuestion("With what is Loki able to control minds?", "Scepter", "Stones", "Weapon","using his hands");
     var question5 = new triviaQuestion("Who was the first major character to die?","Heimdall", "Spider Man", "Ant Man","Black Panther");
     var question6 = new triviaQuestion("what is Captain America's real name?", "Steve Rogers", "Bruce Banner", "Peter Parker", "Bucky Barnes");
     var question7 = new triviaQuestion("How does the Marvel hero Captain America get his powers?", "U.S. military experiment", "Hydra experiment", "chemical exposure", "Bug bite");
     var question8 = new triviaQuestion("Where did Iron Man built his suit", "In a bunker", "In his lab", "In his garage", "At School");
     var question9 = new triviaQuestion("Who created Ant Man suit", "Hank Pym", "Henry Pym", "Rachel Pym", "Scott Lang");
+    var question10 = new triviaQuestion("What device allows Thanos to harness the power of the Infinity Stones?", "Infinity Gauntlet", "Glove compartment", "Metal Hand", "His Purse");
+    var question11 = new triviaQuestion("How many Infinity Stones are there?", "Six", "Seven", "Four", "Fifteen");
+    var question12 = new triviaQuestion("What is Tony Stark’s father’s name?", "Howard", "Andrew", "Peter", "Tony");
+    var question13 = new triviaQuestion("Who are the shapeshifting villains in Captain Marvel?", "Skrulls", "Dust Mites", "Guardians", "Thanos Army");
+    var question14 = new triviaQuestion("How would Groot answer this question?", "I am Groot", "I Kill you", "I am tree", "I am guardian");
+    var question15 = new triviaQuestion("Who is the voice of Rocket?", "Bradley Cooper", "Gerard Butler", "Brad Pitt", "Leonardo DiCaprio");
+    var question16 = new triviaQuestion("What allows transportation between Asgard and Earth in Thor?", "The bifrost", "The Forest", "Transportation pod", "Loki scepter");
+    var question17 = new triviaQuestion("What video game is Thor playing in Avengers: Endgame?", "Fortnite", "Call of Duty", "Minecraft", "Subnautica");
+    var question18 = new triviaQuestion("What store does Carol Danvers crash into in Captain Marvel?", "Blockbuster", "Key Food", "CostCo", "CVS Pharmacy");
+    var question19 = new triviaQuestion("What do the Avengers eat after defeating Loki in New York?", "Shawarma", "Pizza", "Hot Dog", "Nuts");
+    var question20 = new triviaQuestion("What color skin do the Sovereign have?", "Gold", "Pink", "Brown", "Orange");
+    var question21 = new triviaQuestion("What is the name of T’Challa’s sister?", "Shuri", "Ramonda", "Okoye", "Nakia");
+    var question22 = new triviaQuestion("What is Hawkeye’s weapon of choice?", "Bow and arrow", "Sword", "Shield", "Hammer");
+    var question23 = new triviaQuestion("What does Stark Industries manufacture in Iron Man?", "Weapons", "Pots", "Chemicals", "Recycle");
+    var question24 = new triviaQuestion("What is Peter Parker’s aunt’s name?", "May", "April", "June", "Linda");
+    var question25 = new triviaQuestion("Complete the quote from Tony Stark’s daughter: I love you ____", "3000", "2000", "Dad", "Infinity");
+    var question26 = new triviaQuestion("What is Thor the God of?", "Thunder", "Rain", "Love", "Strength");
+    var question27 = new triviaQuestion("Where was Janet van Dyne stuck in Ant-Man?", "The Quantum Realm", "Asgard", "On a space craft", "Inter Universal Meadow");
+    var question28 = new triviaQuestion("What group infiltrated S.H.I.E.L.D. in Captain America: The Winter Soldier?", "Hydra", "Ninja", "Nazi", "Mob");
+    var question29 = new triviaQuestion("Which Infinity Stone does Vision have in his head?", "Mind Stone", "Soul Stone", "Power Stone", "Time Stone");
 
-    arrayOfQuestions.push(question1, question2, question3, question4, question5, question6, question7,question8, question9);
+    arrayOfQuestions.push(question1, question2, question3, question4, question5, question6, question7,question8, question9, question10, question11, question12, question13, question14, question15, question16, question17, question18, question19, question20, question21, question22, question23, question24, question25, question26, question27, question28, question29);
     console.log(arrayOfQuestions);
     // questionSelected = arrayOfQuestions[Math.floor(Math.random()*arrayOfQuestions.length)]
-    startTimer(timeForTest);
-    displayQuestion();
+   
     // displayOptions(question1)
 }
 
@@ -44,7 +66,9 @@ function displayQuestion(){
     arrayOfQuestions.splice(index, 1);
     console.log(arrayOfQuestions);
     var optionArray = [];
-    if(!timerRunning){
+    console.log(timerRunning);
+    if(!questionDisplayed)
+    {
         triviaq.html(questionSelected.question);
         $("#questionHolder").append(triviaq); 
         $("#optionHolder").append('<br>');
@@ -56,9 +80,10 @@ function displayQuestion(){
             optionButton.html(optionArray[i]);
             optionButton.addClass("btn btn-danger");
             optionButton.addClass("optionButtons");
-            $("#optionHolder").append(optionButton);
-         
+            $("#optionHolder").append(optionButton);        
         }
+        $("#optionHolder").append("<br> <br> <br>");
+        questionDisplayed = true;
         $(".optionButtons").on("click", getValue);
     }
 }
@@ -73,18 +98,27 @@ function checkAnswer(userChoice) {
    
     console.log(questionSelected.answer);
     if (userChoice === questionSelected.answer) {
-        console.log("Correct Answer") ; 
-        sessionStorage.noOfWins = parseInt(sessionStorage.noOfWins) + 1 + '';
-        console.log(sessionStorage.noOfWins)
-        // location.reload();
-        displayQuestion();
+        var winMsg = $("<h3>").html("You answered the question correct")
+        $("#questionHolder").prepend(winMsg);
+        correctAnswers++;
+        questionDisplayed = false;
+        if(timerRunning) {
+            $('#details div').empty();
+
+            // $("details").html("");
+            displayQuestion();
+        }
     }
     else {
-        console.log("Incorrect Answer");
-        sessionStorage.noOfLosses = parseInt(sessionStorage.noOfLosses) + 1 + '';
-        console.log(sessionStorage.noOfLosses);
-        // location.reload();
-        displayQuestion();
+        var lossMsg = $("<h3>").html("Sorry wrong answer")
+        $("#questionHolder").prepend(lossMsg);
+        wrongAnswers++;
+        questionDisplayed = false;
+        if(timerRunning) {
+            $("#details div").empty();
+            // $("details").html("");
+            displayQuestion();
+        }
     }
 }
 //Durstenfeld shuffle 
@@ -101,8 +135,9 @@ function shuffleArray(array) {
 function startTimer(duration) {
     var timer = duration, minutes, seconds;
     var intervalId;
+    timerRunning = true;
+    displayQuestion()
     intervalId = setInterval(function () {
-        timerRunning = true;
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -110,10 +145,8 @@ function startTimer(duration) {
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         $("#displayTime").text(minutes + ":" + seconds);
-
         if (--timer < 0) {
             timeUp(intervalId);
-            // document.getElementById("details").setAttribute("disabled","disabled");
         }
     }, 1000);
 }
@@ -122,59 +155,25 @@ function timeUp(interval) {
     clearInterval(interval);
     timerRunning = false;
     $("#displayTime").text("Time up!!!");
+    $("#details div").empty();
+    // var timeUpInfo = $("<h3>");
+    // timeUpInfo.html("Sorry Time is up... Please play again");
+    var noOfWins = $("<h3>");
+    var noOfLosses = $("<h3>");
+    noOfWins.html("You have answered "+correctAnswers+" questions correct");
+    noOfLosses.html("You have answered "+wrongAnswers+" questions incorrectly");
+    // $("#details").append(timeUpInfo);
+    $("#details").append(noOfWins);
+    $("#details").append(noOfLosses);
+
+    
 }
 
 $(document).ready(function () {
     initQuestions();
+    if(!timerRunning) {
+        var myTime= startTimer(timeForTest);
+    }
+    
 });
 
-
-// var intervalId;
-// var clockRunning = false;
-// var time = 0;
-
-// function reset() {
-//     time = 0;
-//     $("#displayTime").text("00:00");
-//   }
-
-// function start() {
-//     if (!clockRunning) {
-//         intervalId = setInterval(count, 1000);
-//         clockRunning = true;
-//     }
-// }
-
-// function stop() {
-//     var timePast = $("#displayTime").text();
-
-//     clearInterval(intervalId);
-//     clockRunning = false;
-// }
-
-// function count() {
-//     time++;
-//     var converted = timeConverter(time);
-//     console.log(converted);
-//     $("#displayTime").text(converted);
-// }
-
-// function timeConverter(t) {
-
-//     var minutes = Math.floor(t / 60);
-//     var seconds = t - (minutes * 60);
-  
-//     if (seconds < 10) {
-//       seconds = "0" + seconds;
-//     }
-  
-//     if (minutes === 0) {
-//       minutes = "00";
-//     }
-//     else if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
-  
-//     return minutes + ":" + seconds;
-//   }
-  
